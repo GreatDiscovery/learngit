737f4a9467fe47fddc22d289a085dfae41f5268e
宋博文 <songbowen@inspur.com>
2019-02-14 15:06:16 +0800

    update MyHashMap

--- src/main/java/jdk/override/util/MyHashMap.java
+++ src/main/java/jdk/override/util/MyHashMap.java
@@ -3,10 +3,9 @@ package jdk.override.util;
 import org.junit.Test;
 
 import java.io.Serializable;
-import java.util.AbstractMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.*;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Function;
@@ -80,6 +79,30 @@ public class MyHashMap<K, V> extends AbstractMap<K, V> implements Map<K, V>, Clo
     static final int hash(Object key) {
         int h;
         return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
+    }
+
+    static Class<?> comparableClassFor(Object x) {
+        if (x instanceof Comparable) {
+            Class<?> c;
+            Type[]  ts, as; Type t;
+            ParameterizedType p;
+            if ((c = x.getClass()) == String.class)
+                return c;
+            if ((ts = c.getGenericInterfaces()) != null) {
+                for (int i = 0; i < ts.length; ++i) {
+                    if (((t = ts[i]) instanceof ParameterizedType) && ((p = (ParameterizedType)t).getRawType() == Comparable.class) &&
+                            (as = p.getActualTypeArguments()) != null &&
+                            as.length == 1 && as[0] == c)
+                        return c;
+                }
+            }
+
+        }
+        return null;
+    }
+
+    static int compareComparables(Class<?> kc, Object k, Object x) {
+        return (x == null || x.getClass() != kc ? 0 : ((Comparable)k).compareTo(x));
     }
 
     static final int tableSizeFor(int cap) {
@@ -193,12 +216,232 @@ public class MyHashMap<K, V> extends AbstractMap<K, V> implements Map<K, V>, Clo
                     threshold = tableSizeFor(t);
             }
             else if (s > threshold)
-//                resize();
+                resize();
             for (Map.Entry<? extends K, ? extends V> e : m.entrySet()) {
                 K key = e.getKey();
                 V value = e.getValue();
-//                putVal(hash(key), key, value, false, evict);
+                putVal(hash(key), key, value, false, evict);
             }
         }
     }
+
+    public int size() { return size; }
+    public boolean isEmpty() { return size == 0; }
+    public V get(Object key) {
+        Node<K, V> e;
+        return (e = getNode(hash(key), key)) == null ? null : e.value;
+    }
+
+    final Node<K, V> getNode(int hash, Object key) {
+        Node<K, V>[] tab; Node<K, V> first, e; int n; K k;
+        if ((tab = table) != null && (n = tab.length) > 0 &&
+                (first = tab[(n - 1) & hash]) != null) {
+
+        }
+        return null;
+    }
+    public boolean containsKey(Object key) { return getNode(hash(key), key) != null; }
+    public V put(K key, V value) {
+        return putVal(hash(key), key, value, false, true);
+    }
+
+    final V putVal(int hash, K key, V value, boolean onlyIfAbsent, boolean evice) {
+        Node<K, V>[] tab; Node<K, V> p; int n, i;
+        if ((tab = table) == null || (n = tab.length) == 0 )
+            n = (tab = resize()).length;
+        // 如果hash位置为null，直接放入
+        if ((p = tab[i = (n - 1) & hash]) == null)
+            tab[i] = newNode(hash, key, value, null);
+        else {
+            Node<K, V> e; K k;
+            // 如果该位置与要存入的node相等，直接返回该node
+            if (p.hash == hash &&
+                    ((k = p.key) == key || (key != null && key.equals(k))))
+                e = p;
+            else if (p instanceof TreeNode) {
+                e = ((TreeNode<K, V>)p).putTreeVal(this, tab, hash, key, value);
+            }
+            else {
+                for (int binCount = 0; ; ++binCount) {
+                    if ((e = p.next) == null) {
+                        p.next = newNode(hash, key, value, null);
+                        if (binCount >= TREEIFY_THRESHOLD - 1) {
+                            treeifyBin(tab, hash);
+                        }
+                        break;
+                    }
+                    if (e.hash == hash && ((k = e.key) == key || (key != null && key.equals(k))))
+                        break;
+                    p = e;
+                }
+            }
+            if (e != null) {
+                V oldValue = e.value;
+                return oldValue;
+            }
+        }
+        ++modCount;
+        if (++size > threshold)
+            resize();
+        return null;
+    }
+
+    final Node<K, V>[] resize() {
+        // 先对oldTab进行判断，主要进行容量和阈值的计算
+        Node<K, V>[] oldTab = table;
+        int oldCap = (oldTab == null) ? 0 : oldTab.length;
+        int oldThr = threshold;
+        int newCap, newThr = 0;
+        // 如果oldCap大于0，先判断有没有超过最大容量
+        if (oldCap > 0) {
+            if (oldCap >= MAXIMUM_CAPACITY) {
+                threshold = Integer.MAX_VALUE;
+                return oldTab;
+            }
+            // 如果没有超过最大容量，进行移位后再判断超过最大容量
+            else if ((newCap = oldCap << 1) < MAXIMUM_CAPACITY && oldCap >= DEFAULT_INITAL_CAPACITY)
+                newThr = oldThr << 1;
+        }
+        // 如果容量为0，但是有初始的threshold值，可以不用默认值
+        else if (oldThr > 0)
+            newCap = oldThr;
+        else {
+            newCap = DEFAULT_INITAL_CAPACITY;
+            newThr = (int) (DEFAULT_INITAL_CAPACITY * DEFAULT_INITAL_CAPACITY);
+        }
+        // 重新处理阈值
+        if (newThr == 0) {
+            float ft = (float) newCap * loadFactor;
+            newThr = (newCap < MAXIMUM_CAPACITY && ft < (float)MAXIMUM_CAPACITY ? (int)ft : Integer.MAX_VALUE);
+        }
+        threshold = newThr;
+        // new一个新的数组，可以开始搬数据了
+        Node<K, V>[] newTab = (Node<K, V>[])new Node[newCap];
+        table = newTab;
+        if (oldTab != null) {
+            for (int j = 0; j < oldCap; ++j) {
+                Node<K, V> e;
+                if ((e = oldTab[j]) != null) {
+                    oldTab[j] = null;
+                    if (e.next == null)
+                        // 如果只有单个node，直接存入新数组
+                        newTab[e.hash & (newCap - 1)] = e;
+                    else if (e instanceof TreeNode)
+                        // 如果是treeNode，重新拆解node
+                        ((TreeNode<K, V>)e).split(this, newTab, j, oldCap);
+                    else {
+                        // 为什么需要这么麻烦？为了保证拷贝过去的链表保持原顺序
+                        Node<K, V> loHead = null, loTail = null;
+                        Node<K, V> hiHead = null, hiTail = null;
+                        Node<K, V> next;
+                        do {
+                            next = e.next;
+                            // 原索引
+                            if ((e.hash & oldCap) == 0) {
+                                if (loTail == null)
+                                    loHead = e;
+                                else
+                                    loTail.next = e;
+                                loTail = e;
+                            }
+                            // 原索引 + oldCap
+                            else {
+                                if (hiTail == null)
+                                    hiHead = e;
+                                else
+                                    hiTail.next = e;
+                                hiTail = e;
+                            }
+                        } while ((e = next ) != null);
+                        // 先组成链表，再放入桶内
+                        if (loTail != null) {
+                            loTail.next = null;
+                            newTab[j] = loHead;
+                        }
+                        if (hiTail != null) {
+                            hiTail.next = null;
+                            newTab[j + oldCap] = hiHead;
+                        }
+                    }
+                }
+            }
+
+        }
+        return newTab;
+    }
+
+    final void treeifyBin(Node<K, V>[] tab, int hash) {
+
+    }
+
+    /*-------------LinkedHashMap support---------------*/
+    Node<K, V> newNode(int hash, K key, V value, Node<K, V> next) {
+        return new Node<>(hash, key, value, next);
+    }
+
+    static final class TreeNode<K, V> extends LinkedHashMap.Entry<K, V> {
+        TreeNode<K, V> parent;
+        TreeNode<K, V> left;
+        TreeNode<K, V> right;
+        TreeNode<K, V> prev;
+        boolean red;
+        TreeNode(int hash, K key, V val, MyHashMap.Node<K,V> next) {
+            super(hash, key, val, next);
+        }
+
+        final TreeNode<K, V> root() {
+            for (TreeNode<K, V> r = this, p;;) {
+                if ((p = r.parent) == null) {
+                    return r;
+                }
+                r = p;
+            }
+        }
+
+        final TreeNode<K, V> putTreeVal(MyHashMap<K, V> map, Node<K, V>[] tab, int h, K k, V v) {
+
+            Class<?> kc = null;
+            boolean searched = false;
+            TreeNode<K, V> root = (parent != null) ? root(): this;
+            for (TreeNode<K ,V> p = root;;) {
+                int dir, ph; K pk;
+                // 此节点的hash值与h不同，h是调用hash函数算出来的，而p.hash是节点真正的hash值，在此处比较是为了方便放入左子树还是右子树
+                if ((ph = p.hash) > h )
+                    dir = -1;
+                else if (ph < h)
+                    dir = 1;
+                // 同一个对象或者值相等都可以
+                else if ((pk = p.key) == k || (k != null && k.equals(pk)))
+                    return p;
+                else if ((kc == null && (kc = comparableClassFor(k)) == null) || (dir = compareComparables(kc, k, pk)) == 0)) {
+                    if (!searched) {
+                        TreeNode<K, V> q, ch;
+                        searched = true;
+                        if (((ch = p.left) != null &&
+                                (q = ch.find(h, k, kc)) != null) ||
+                                ((ch = p.right) != null &&
+                                        (q= ch.find(h, k, kc)) != null))
+                            return q;
+                    }
+                    dir = tieBreakOrder(k, pk);
+                }
+                TreeNode<K, V> xp = p;
+                if ((p = (dir <= 0) ? p.left : p.right) == null) {
+                    Node<K, V> xpn = xp.next;
+                    TreeNode<K, V> x = map.newNode(h, k, v, xpn);
+                    if (dir <= 0)
+                        xp.left =x;
+                    else
+                        xp.right = x;
+                    xp.next = x;
+                    x.parent = x.prev = xp;
+                    if (xpn != null)
+                        ((TreeNode<K, V>)xpn).prev = x;
+                    moveRootToFront(tab, balanceInsertion(root, x));
+                    return null;
+                }
+            }
+        }
+    }
+
 }
